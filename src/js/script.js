1c18de1ef8f942d1a1c1b861bfa8a54a2c976047
Welington <welingtoncharnetjr@gmail.com>
2025-05-15 22:10:59 -0300

    Avançando No js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,29 @@
 function addtask() {
 
     var input = document.getElementById("input-tarefa")
-    console.log(input.value)
+    //console.log(input)
     //alert("Função de adicionar tarefa")
+
+    var inputValue = input.value
+
+    //Limpar o input
+    input.value = ""
+
+    var list = document.getElementById("lista")
+    //Criar um elemento li
+    var li = document.createElement("li")
+
+    // Adicionar o valor do INPUT para o LI
+    //li.innerText = inputValue
+    li.innerHTML = `${inputValue}<button onclick= 'this.parentElement.remove()'> </button>`;
+
+    //Adicionar função como concluida
+    li.onclick = function() {
+        li.className = "tarefa-concluida";
+    }
+
+    //Adicionar o LI na lista de tarefas
+    list.appendChild(li)
+
+    console.log(inputValue)
 }
